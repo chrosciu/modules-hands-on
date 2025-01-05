a0cc369e62261c28bfb33bee726937a19e470c3c
Marcin Chrost <mchrost82@gmail.com>
2025-01-05 12:23:33 +0100

    Step 1 - two modules with dependency from one to another

---
+++ main/src/main/java/eu/chrost/main/Main.java
@@ -0,0 +1,11 @@
+package eu.chrost.main;
+
+import eu.chrost.math.Calculator;
+
+class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println("Sum: " + calculator.add(2, 3));
+        System.out.println("Difference: " + calculator.subtract(5, 3));
+    }
+}
---
+++ math/src/main/java/eu/chrost/math/Calculator.java
@@ -0,0 +1,11 @@
+package eu.chrost.math;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
