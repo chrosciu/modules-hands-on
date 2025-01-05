b9672538ad94613bf8e99ae296bbcc6c05b6a435
Marcin Chrost <mchrost82@gmail.com>
2025-01-05 13:15:22 +0100

    Step 4 - reflective access with opens

--- main/src/main/java/eu/chrost/main/Main.java
+++ main/src/main/java/eu/chrost/main/Main.java
@@ -3,14 +3,24 @@ package eu.chrost.main;
 import eu.chrost.math.Operation;
 import eu.chrost.utils.Logger;
 
+import java.lang.reflect.Field;
 import java.util.ServiceLoader;
 
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Logger logger = new Logger();
         ServiceLoader<Operation> operations = ServiceLoader.load(Operation.class);
         for (Operation op : operations) {
             logger.log(Integer.toString(op.apply(10, 5)));
         }
+
+        Class<?> personClass = Class.forName("eu.chrost.model.Person");
+        Object personInstance = personClass.getDeclaredConstructor().newInstance();
+
+        // Odczytanie prywatnego pola "name"
+        Field nameField = personClass.getDeclaredField("name");
+        nameField.setAccessible(true); // Uzyskanie dostępu refleksyjnego
+        String name = (String) nameField.get(personInstance);
+        System.out.println("Name: " + name);
     }
 }
---
+++ model/src/main/java/eu/chrost/model/Person.java
@@ -0,0 +1,5 @@
+package eu.chrost.model;
+
+public class Person {
+    private final String name = "Marcin";
+}
